a7c0048e59df12ad8103041bf6be770327b62129
salah <farzin>
2021-02-11 01:13:25 +0330

    fix too many request sent from react to server bug

--- frontend-reactjs/src/App.tsx
+++ frontend-reactjs/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {FC, lazy, Suspense, useEffect, useRef, useState} from 'react';
+import React, {FC, lazy, Suspense, useEffect, useState} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {useSelector} from 'react-redux';
 import {RootState} from './rootReducer';
@@ -8,6 +8,7 @@ import {AuthResponse} from "./interfaces/authResponse";
 import {useAppDispatch} from "./store";
 import {login} from "./features/auth/authSlice";
 import {showAlert} from "./util";
+import Loader from "./features/loader/Loader";
 
 const Auth = lazy(() => import('./features/auth/Auth'));
 const Product = lazy(() => import('./features/product/Product'));
@@ -15,36 +16,35 @@ const Product = lazy(() => import('./features/product/Product'));
 const App: FC = () => {
     const dispatch = useAppDispatch();
     const [loading, setLoading] = useState(false);
-    const willMount = useRef(true);
 
     const isLoggedIn = useSelector(
         (state: RootState) => state.auth.isAuthenticated
     );
 
+    // just run once
     useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const code = urlParams.get('code');
 
-    });
-            const urlParams = new URLSearchParams(window.location.search);
-            const code = urlParams.get('code');
-
-            if (!isLoggedIn && code) {
-                setLoading(true);
-                http
-                    .get<User, AuthResponse>('auth/github?code=' + code,)
-                    .then((res) => {
-                        dispatch(login(res));
-                        window.location.href = '/';
-                    })
-                    .catch((error) => {
-                        showAlert(error.message, 'error');
-                    }).finally(() => setLoading(false));
-            }
+        if (!isLoggedIn && code) {
+            setLoading(true);
+            http
+                .get<User, AuthResponse>('auth/github?code=' + code,)
+                .then((res) => {
+                    dispatch(login(res));
+                    window.location.href = '/';
+                })
+                .catch((error) => {
+                    showAlert(error.message, 'error');
+                }).finally(() => setLoading(false));
+        }
+    }, []);
 
     return (
         <Router>
             <Switch>
                 <Route path="/">
-                    <Suspense fallback={<p>Loading...</p>}>
+                    <Suspense fallback={<Loader/>}>
                         {isLoggedIn ? <Product/> : <Auth/>}
                     </Suspense>
                 </Route>
