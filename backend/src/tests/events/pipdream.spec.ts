b54c9a7c2c489c54f1a3a112d0c8ba2693a41a62
salah <farzin>
2021-02-11 03:46:34 +0330

    add Pipdream test (webhook)

---
+++ backend/src/tests/events/pipdream.spec.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const endpoint: any = process.env.PD_ENDPOINT;
+
+describe('Test Pipdream endpoint with a test event', () => {
+
+    it("SEND this is the product-inventory app", async done => {
+        const result = await axios
+            .post(endpoint, {
+                message: "This is the product-inventory app test send at" + Date.now()
+            }, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+        expect(result.status).toBe(200);
+        expect(result.data.success).toBe(true);
+
+        done();
+    });
+});
